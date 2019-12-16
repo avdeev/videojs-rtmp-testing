96ccca2b33340009cdf9812783ae12ce632099fc
Alexey Avdeev <aad.jerry@gmail.com>
2019-12-16 18:56:40 +0300

    Updates

---
+++ precache-manifest.6505eb7a2541c8d519eabddf81f57d36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5903a73605e433885814a5edaba577",
+    "url": "/videojs-rtmp-testing/index.html"
+  },
+  {
+    "revision": "b064762cd40857472775",
+    "url": "/videojs-rtmp-testing/static/css/2.e3795789.chunk.css"
+  },
+  {
+    "revision": "974b53f56792fcc3e282",
+    "url": "/videojs-rtmp-testing/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "b064762cd40857472775",
+    "url": "/videojs-rtmp-testing/static/js/2.bbe28502.chunk.js"
+  },
+  {
+    "revision": "527b39bc246ae2252e39c047b5f6c865",
+    "url": "/videojs-rtmp-testing/static/js/2.bbe28502.chunk.js.LICENSE"
+  },
+  {
+    "revision": "974b53f56792fcc3e282",
+    "url": "/videojs-rtmp-testing/static/js/main.a5b7a49c.chunk.js"
+  },
+  {
+    "revision": "55d3afa1ceda3898413c",
+    "url": "/videojs-rtmp-testing/static/js/runtime-main.036f2119.js"
+  }
+]);
